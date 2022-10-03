0e44829f96935ea41357d2269ec72b0c0de0d1a5
dazon <gamerpenguin2005@gmail.com>
2022-10-03 22:36:41 +0000

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+    import Header from "../lib/components/Header.svelte";
+    import Styles from "../lib/styles/main.css"
+</script>
+<Header></Header>
+<main>
+    <slot></slot>
+</main>
---
+++ src/routes/uses/+page.md
@@ -0,0 +1,6 @@
+# Uses
+
+```js
+// asdf
+console.log("a");
+```
