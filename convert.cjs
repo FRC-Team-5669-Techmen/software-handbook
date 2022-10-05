949752e90147d013eeae784bd4de8ff91ac34ca2
dazon <gamerpenguin2005@gmail.com>
2022-10-05 04:14:08 +0000

    yeah so

--- convert.cjs
+++ convert.cjs
@@ -1,20 +1,10 @@
 var glob = require("glob")
 var fs = require('fs')
+var SimpleMarkdown = require("simple-markdown");
+var markdown = require("markdown-it")
+var md = new markdown()
 
 //get summary file in docs folder
-glob("docs/SUMMARY.md", function (er, files) {
-  console.log(files)
-}
-)
-// options is optional
-glob("docs/**/*", 
-{mark: true}, function (er, files) {
-  // files is an array of filenames.
-  // If the `nonull` option is set, and nothing
-  // was found, then files is ["**/*.js"]
-  // er is an error object or null.
-  files.forEach(e => {
-    //use fs to see if it is a directory
-    console.log(fs.statSync(e).isDirectory() + " " + e)
-  })
-})
+var summary = fs.readFileSync("./docs/SUMMARY.md", 'utf8')
+const content = md.parse(summary)
+console.log( JSON.stringify(content, null, "\t"))
--- docs/SUMMARY.md
+++ docs/SUMMARY.md
@@ -8,13 +8,3 @@
 - [Documentation](../docs/software/reference/overview.md)
 ## Guides
 - [Guides](../docs/software/guides/overview.md)
-
-# Electrical
-[Overview](../docs/electrical/README.md)
-## Getting Started
-- [Installation](../docs/electrical/getting-started/installation.md)
-- [GitHub](../docs/electrical/getting-gtarted/github.md)
-## Reference
-- [Documentation](../docs/electrical/geference/overview.md)
-## Guides
-- [Guides](../docs/electrical/guides/overview.md)
